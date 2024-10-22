d2bda694fc0ab1dc152e5ef13b3f004a99938db6
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2024-10-22 16:44:28 +0200

    add rectification

--- dist/build.js
+++ dist/build.js
@@ -17562,28 +17562,12 @@ var require_maplibre_gl_directions = __commonJS({
           return o;
         }
         return r;
-<<<<<<< HEAD
       }())({
         1: [function(require2, module3, exports3) {
           "use strict";
-          var polyline = {};
+          var polyline2 = {};
           function py2_round(value) {
             return Math.floor(Math.abs(value) + 0.5) * (value >= 0 ? 1 : -1);
-=======
-      }())({ 1: [function(require2, module3, exports3) {
-        "use strict";
-        var polyline2 = {};
-        function py2_round(value) {
-          return Math.floor(Math.abs(value) + 0.5) * (value >= 0 ? 1 : -1);
-        }
-        function encode(current, previous, factor) {
-          current = py2_round(current * factor);
-          previous = py2_round(previous * factor);
-          var coordinate = current - previous;
-          coordinate <<= 1;
-          if (current - previous < 0) {
-            coordinate = ~coordinate;
->>>>>>> bdd3a43f0a524c4d66399bc662fa9f5f1b8c5c18
           }
           function encode(current, previous, factor) {
             current = py2_round(current * factor);
@@ -17601,8 +17585,7 @@ var require_maplibre_gl_directions = __commonJS({
             output += String.fromCharCode(coordinate + 63);
             return output;
           }
-<<<<<<< HEAD
-          polyline.decode = function(str, precision) {
+          polyline2.decode = function(str, precision) {
             var index = 0, lat = 0, lng = 0, coordinates = [], shift = 0, result = 0, byte = null, latitude_change, longitude_change, factor = Math.pow(10, Number.isInteger(precision) ? precision : 5);
             while (index < str.length) {
               byte = null;
@@ -17627,7 +17610,7 @@ var require_maplibre_gl_directions = __commonJS({
             }
             return coordinates;
           };
-          polyline.encode = function(coordinates, precision) {
+          polyline2.encode = function(coordinates, precision) {
             if (!coordinates.length) {
               return "";
             }
@@ -17646,102 +17629,25 @@ var require_maplibre_gl_directions = __commonJS({
               flipped2.push([coord[1], coord[0]]);
             }
             return flipped2;
-=======
-          output += String.fromCharCode(coordinate + 63);
-          return output;
-        }
-        polyline2.decode = function(str, precision) {
-          var index = 0, lat = 0, lng = 0, coordinates = [], shift = 0, result = 0, byte = null, latitude_change, longitude_change, factor = Math.pow(10, Number.isInteger(precision) ? precision : 5);
-          while (index < str.length) {
-            byte = null;
-            shift = 0;
-            result = 0;
-            do {
-              byte = str.charCodeAt(index++) - 63;
-              result |= (byte & 31) << shift;
-              shift += 5;
-            } while (byte >= 32);
-            latitude_change = result & 1 ? ~(result >> 1) : result >> 1;
-            shift = result = 0;
-            do {
-              byte = str.charCodeAt(index++) - 63;
-              result |= (byte & 31) << shift;
-              shift += 5;
-            } while (byte >= 32);
-            longitude_change = result & 1 ? ~(result >> 1) : result >> 1;
-            lat += latitude_change;
-            lng += longitude_change;
-            coordinates.push([lat / factor, lng / factor]);
           }
-          return coordinates;
-        };
-        polyline2.encode = function(coordinates, precision) {
-          if (!coordinates.length) {
-            return "";
-          }
-          var factor = Math.pow(10, Number.isInteger(precision) ? precision : 5), output = encode(coordinates[0][0], 0, factor) + encode(coordinates[0][1], 0, factor);
-          for (var i = 1; i < coordinates.length; i++) {
-            var a = coordinates[i], b = coordinates[i - 1];
-            output += encode(a[0], b[0], factor);
-            output += encode(a[1], b[1], factor);
-          }
-          return output;
-        };
-        function flipped(coords) {
-          var flipped2 = [];
-          for (var i = 0; i < coords.length; i++) {
-            var coord = coords[i].slice();
-            flipped2.push([coord[1], coord[0]]);
-          }
-          return flipped2;
-        }
-        polyline2.fromGeoJSON = function(geojson, precision) {
-          if (geojson && geojson.type === "Feature") {
-            geojson = geojson.geometry;
-          }
-          if (!geojson || geojson.type !== "LineString") {
-            throw new Error("Input must be a GeoJSON LineString");
-          }
-          return polyline2.encode(flipped(geojson.coordinates), precision);
-        };
-        polyline2.toGeoJSON = function(str, precision) {
-          var coords = polyline2.decode(str, precision);
-          return {
-            type: "LineString",
-            coordinates: flipped(coords)
-          };
-        };
-        if (typeof module3 === "object" && module3.exports) {
-          module3.exports = polyline2;
-        }
-      }, {}], 2: [function(require2, module3, exports3) {
-        "use strict";
-        var isObj = require2("is-obj");
-        var hasOwnProperty = Object.prototype.hasOwnProperty;
-        var propIsEnumerable = Object.prototype.propertyIsEnumerable;
-        function toObject(val) {
-          if (val === null || val === void 0) {
-            throw new TypeError("Sources cannot be null or undefined");
->>>>>>> bdd3a43f0a524c4d66399bc662fa9f5f1b8c5c18
-          }
-          polyline.fromGeoJSON = function(geojson, precision) {
+          polyline2.fromGeoJSON = function(geojson, precision) {
             if (geojson && geojson.type === "Feature") {
               geojson = geojson.geometry;
             }
             if (!geojson || geojson.type !== "LineString") {
               throw new Error("Input must be a GeoJSON LineString");
             }
-            return polyline.encode(flipped(geojson.coordinates), precision);
+            return polyline2.encode(flipped(geojson.coordinates), precision);
           };
-          polyline.toGeoJSON = function(str, precision) {
-            var coords = polyline.decode(str, precision);
+          polyline2.toGeoJSON = function(str, precision) {
+            var coords = polyline2.decode(str, precision);
             return {
               type: "LineString",
               coordinates: flipped(coords)
             };
           };
           if (typeof module3 === "object" && module3.exports) {
-            module3.exports = polyline;
+            module3.exports = polyline2;
           }
         }, {}],
         2: [function(require2, module3, exports3) {
